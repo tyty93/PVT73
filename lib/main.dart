526bd6aedacd25b79350863808b804f141a8afad
LazarosTriantafillidis <lazos.tri@gmail.com>
2025-04-15 21:53:41 +0200

    remove test lazos
    hej

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       home: MultiProvider(
         providers: [
           Provider(create: (_) => AuthRepository()),
-          // Inject AuthRepository into both ViewModels  Lazaro
+          // Inject AuthRepository into both ViewModels
           ChangeNotifierProvider(
             create: (context) => AuthViewmodel(authRepository: context.read<AuthRepository>()),
           ),
