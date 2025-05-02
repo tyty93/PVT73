e744a2dec09100ab51951b790c5cc82752a4b518
Alex <alexrosenk@hotmail.se>
2025-05-02 09:08:57 +0200

    added unit tests for event repository and viewmodel

---
+++ test/event_repository_test.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:flutter_application_1/data/models/event.dart';
+import 'package:flutter_application_1/data/repositories/event_repository.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'event_repository_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  late EventRepository eventRepository;
+  late MockClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockClient();
+    eventRepository = EventRepositoryImpl(client: mockHttpClient);
+  });
+
+  group("Event repository - ", () {
+    group("fetchEvents", () {
+      test(
+        "Given EventRepository class, when fetchEvents is called and status code is HttpStatus.ok then a Future<List<Event>> should be returned ",
+        () async {
+          // Stub
+          when(
+            mockHttpClient.get(
+              Uri.parse("https://group-3-75.pvt.dsv.su.se/demo/events"),
+            ),
+          ).thenAnswer(
+            (_) async => Response('''
+              [
+                  {
+                    "id":53,
+                    "name":"Event 4",
+                    "description":"Description of event 1",
+                    "date":"2025-05-01T14:00:00"
+                  },
+                  {
+                    "id":54,
+                    "name":"Event 4",
+                    "description":"Description of event 1",
+                    "date":"2025-05-05T14:00:00"
+                  }
+              ]
+              ''',
+              HttpStatus.ok,
+            ),
+          );
+
+          final actual = await eventRepository.fetchEvents();
+
+          expect(actual, isA<List<Event>>());
+        },
+      );
+
+      test(
+        "Given EventRepository class, when fetchEvents is called and status code is NOT HttpStatus.ok then an exception should be thrown",
+        () async {
+          when(
+            mockHttpClient.get(
+              Uri.parse("https://group-3-75.pvt.dsv.su.se/demo/events"),
+            ),
+          ).thenAnswer((_) async => Response('Not found', HttpStatus.notFound));
+
+          final actual = eventRepository.fetchEvents();
+
+          expect(actual, throwsException);
+        },
+      );
+    });
+    group("deleteEvent", () {
+      test("Should successfully delete an existing event", () {
+
+      });
+    });
+  });
+}
---
+++ test/events_page_viewmodel_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_application_1/data/repositories/event_repository.dart';
+import 'package:flutter_application_1/ui/event/event_page_viewmodel.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockEventRepository extends Mock implements EventRepository {}
+
+void main() {
+  late MockEventRepository mockEventRepository;
+  late EventsViewmodel viewModel;
+
+  setUp(() {
+    mockEventRepository = MockEventRepository();
+    viewModel = EventsViewmodel(eventRepository: mockEventRepository);
+  });
+
+  group('ViewModel construction - ', () {
+    test(
+      "Given ViewModel class, When instantiated, Then hasLoadedEvents should be false",
+          () {
+        final bool expected = false;
+        final bool actual = viewModel.hasLoadedEvents;
+
+        expect(actual, expected);
+      },
+    );
+  });
+}
