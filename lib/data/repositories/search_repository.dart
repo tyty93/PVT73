c493386320c8cc5caaf9016b21dd81c52073f9a3
mxi7615 <lecoq6653@gmail.com>
2025-05-08 14:53:32 +0200

    Added search_repository

---
+++ lib/data/repositories/search_repository.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import '../Friend Model/stranger.dart';
+
+import 'dart:developer';
+
+abstract class SearchRepository{
+  Future<List<Stranger>> searchUsers(int id, String searchString);
+}
+
+class SearchRepositoryImpl implements SearchRepository{
+  final http.Client client;
+  final String baseUrl = "http://10.0.2.2:8080/demo";
+
+  SearchRepositoryImpl({http.Client? client}) : client = client ?? http.Client();
+
+  @override
+  Future<List<Stranger>> searchUsers(int id, String searchString) async{
+    log('Crashed here 1');
+    final response = await client.get( Uri.parse('$baseUrl/friend/searchNonFriends?uid=$id&search=$searchString'));
+    log('Crashed here 1');
+    if(response.statusCode == HttpStatus.ok){
+      final String jsonString = response.body;
+      final List<dynamic> strangersJson = jsonDecode(jsonString);
+      final List<Stranger> users = [];
+      for(Map<String,dynamic> json in strangersJson){
+        users.add(Stranger.fromJson(json));
+        log('found $json');
+      }
+      return users;
+    }
+    else{
+      throw Exception("Failed to fetch strangers");
+    }
+  }
+}
