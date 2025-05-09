c0110db7d89a87f525a9308aca1e1949466f3d02
Alex <alexrosenk@hotmail.se>
2025-05-10 00:39:43 +0200

    Added nested navigation for home page (is unexpectedly also working for event page)

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -4,18 +4,23 @@ import 'package:flutter_application_1/routing/routes.dart';
 import 'package:flutter_application_1/ui/event/event_page.dart';
 import 'package:go_router/go_router.dart';
 
+import '../data/models/event.dart';
 import '../ui/auth/viewmodels/auth_viewmodel.dart';
 import '../ui/auth/widgets/auth_page.dart';
 import '../ui/event_info/event_info_page.dart';
 import '../ui/home/home_page.dart';
+// Shaared detail sreens from multiple tabs? see https://dev.to/7twilight/mastering-nested-navigation-in-flutter-with-gorouter-and-a-bottom-nav-bar-555l
 // TODO: add navigatorkeys for each branch, try NoTransitionPages using pageBuilder for no 'android transition' screen from default MaterialPage
 // see https://codewithandrea.com/articles/flutter-bottom-navigation-bar-nested-routes-gorouter/ for more info
 // TODO: test if android back button works on deeper nested pages within any bottomnavbr item's branch.
 final _rootNavigatorKey = GlobalKey<NavigatorState>(debugLabel: 'root');
+final _shellNavigatorAKey = GlobalKey<NavigatorState>(debugLabel: 'shellA');
+final _shellNavigatorBKey = GlobalKey<NavigatorState>(debugLabel: 'shellB');
+final _shellNavigatorCKey = GlobalKey<NavigatorState>(debugLabel: 'shellC');
 
 // Use StatefulShellRoute for bottom navigation bar, which will show a UI shell
 // wrapping the main content and preventing the navbar from rebuilding each time we navigate to new page
-// todo: don't use a createRouter method..  refactor GoRouter instance into singleton DI with  provider to avoid rebuild on hot reload
+// todo: don't use a createRouter method..  refactor GoRouter instance into singleton DI with  provider to avoid rebuild on hot reload (see https://github.com/bizz84/nested_navigation_examples/blob/main/examples/gorouter/lib/main.dart)
 GoRouter createRouter(AuthViewmodel authViewmodel) {
   return GoRouter(
     navigatorKey: _rootNavigatorKey,
@@ -40,6 +45,7 @@ GoRouter createRouter(AuthViewmodel authViewmodel) {
         ),
         branches: [
           StatefulShellBranch(
+            navigatorKey: _shellNavigatorAKey,
             routes: [
               GoRoute(
                 path: Routes.homePage,
@@ -47,13 +53,17 @@ GoRouter createRouter(AuthViewmodel authViewmodel) {
                 routes: [
                   GoRoute(
                     path: Routes.eventDetailPath,
-                    pageBuilder:  (context, state) => NoTransitionPage(child: EventInfoPage(eventId: int.parse(state.pathParameters['eventId']!))) // eveninfopage to the specific event by eventId, but wshould use correct path string
+                    pageBuilder:  (context, state) {
+                      final event = state.extra as Event;
+                      return NoTransitionPage(child: EventInfoPage(event: event));
+                    }
                   ),
                 ],
               )
             ],
           ),
           StatefulShellBranch(
+            navigatorKey: _shellNavigatorBKey,
             routes: [
               GoRoute(
                 path: Routes.eventsPage,
@@ -62,6 +72,7 @@ GoRouter createRouter(AuthViewmodel authViewmodel) {
             ],
           ),
           StatefulShellBranch(
+            navigatorKey: _shellNavigatorCKey,
             routes: [
               GoRoute(
                 path: Routes.profilePage,
