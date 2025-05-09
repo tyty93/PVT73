2624eb9f6ff38b8a24c51093b670a94505fc81d1
Alex <alexrosenk@hotmail.se>
2025-05-10 00:46:14 +0200

    Added comment

--- lib/data/repositories/event_info_repository.dart
+++ lib/data/repositories/event_info_repository.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 import '../models/event_info.dart';
 
 // todo commented out because no need to fetch event by id, we can pass the data directly from the home page
+// todo othewise, can just use EventRepository which already is the Single Source of Truth for all event-related data
 /*abstract class EventInfoRepository {
   Future<EventInfo> fetchEventById(int id);
 }
