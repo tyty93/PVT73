afb725ba366401e9a3c9fbedda17cf024d6d20e2
mxi7615 <lecoq6653@gmail.com>
2025-05-14 11:38:32 +0200

    Fixade en merge conflict

--- lib/ui/auth/widgets/auth_page.dart
+++ lib/ui/auth/widgets/auth_page.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:provider/provider.dart';
-import '../../friends/friends_page/friends_page.dart';
-import '../viewmodels/auth_viewmodel.dart';
-=======
->>>>>>> 32298f159d0b87186b0c4726701390f3e75823c8
 import 'login_or_register_page.dart';
 
 // todo: refactor by merging with login/register page
@@ -13,20 +7,6 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return Scaffold(
-      body: Consumer<AuthViewmodel>(
-        builder: (context, viewModel, _) {
-          if (viewModel.isLoggedIn) {
-            return const FriendPageScreen();
-          } else {
-            return const LoginOrRegisterPage();
-          }
-        },
-      ),
-    );
-=======
     return const LoginOrRegisterPage();
->>>>>>> 32298f159d0b87186b0c4726701390f3e75823c8
   }
 }
