9d40a007efe67ba91fa12bae04178ba4e1519273
mida1482 <mirre.dan@hotmail.com>
2025-05-14 11:51:59 +0200

    create_event.dart added, and some new dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,23 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/ui/auth/viewmodels/auth_viewmodel.dart';
-import 'package:flutter_application_1/ui/auth/viewmodels/login_or_register_viewmodel.dart';
-import 'package:flutter_application_1/ui/auth/widgets/auth_page.dart';
-import 'package:flutter_application_1/ui/common/theme/theme.dart';
-import 'package:flutter_application_1/ui/home/home_page_viewmodel.dart';
-import 'package:provider/provider.dart';
-
-import 'data/repositories/auth_repository.dart';
-import 'firebase_options.dart';
+import 'package:flutter_application_1/ui/create_event.dart';
+import 'package:intl/intl_standalone.dart' if (dart.library.html) 'package:intl/intl_browser.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await findSystemLocale();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'The App',
-      home: MultiProvider(
-        providers: [
-          Provider(create: (_) => AuthRepository()),
-          // Inject AuthRepository into both ViewModels
-          ChangeNotifierProvider(
-            create: (context) => AuthViewmodel(authRepository: context.read<AuthRepository>()),
-          ),
-          ChangeNotifierProvider(
-            create: (context) => LoginOrRegisterViewmodel(authRepository: context.read<AuthRepository>()),
-          ),
-          ChangeNotifierProvider(
-              create: (context) => HomeViewmodel(authRepository: context.read<AuthRepository>()),
-          )
-        ],
-        child: const AuthPage(),
-      ),
-      themeMode: ThemeMode.system,
-      theme: MaterialTheme.lightTheme,
-      darkTheme: MaterialTheme.darkTheme,
+      home: CreateEvent(),
     );
+
   }
 }
