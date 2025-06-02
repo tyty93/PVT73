a702f06f9cd47efc80331841dee9bb82a727f793
mida1482 <mirre.dan@hotmail.com>
2025-06-02 14:33:39 +0200

    Changed backrgound color on friend screen and also added create new event button on event list page

--- lib/ui/event/event_page.dart
+++ lib/ui/event/event_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/ui/event/event_page_viewmodel.dart';
 import 'package:provider/provider.dart';
+import '../home/create_event.dart';
 import 'event_card.dart';
 
 /* Should show all events available to sign up for. By clicking an event, go to EventInfo which should have a register button */
@@ -10,9 +11,7 @@ class EventPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Events"),
-      ),
+      appBar: AppBar(title: Text("Events")),
       body: Consumer<EventsViewmodel>(
         builder: (context, viewModel, _) {
           if (viewModel.availableEvents == null) {
@@ -26,17 +25,41 @@ class EventPage extends StatelessWidget {
             onRefresh: viewModel.refreshEvents,
             child: ListView.builder(
               itemBuilder: (context, index) {
-                return EventCard(
-                    event: events[index],
-                    index: index,
-                  );
+                return EventCard(event: events[index], index: index);
               },
-              itemCount: events.length
+              itemCount: events.length,
             ),
           );
-        }
+        },
+      ),
+
+      floatingActionButton: Container(
+        width: 56,
+        height: 56,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            colors: [
+              Theme.of(context).colorScheme.inversePrimary,
+              Theme.of(context).colorScheme.primary,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+
+        child: FloatingActionButton(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CreateEvent()),
+            );
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
 }
-
--- lib/ui/friends/friends_page/friends_page.dart
+++ lib/ui/friends/friends_page/friends_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as theme;
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/data/models/user.dart';
 import 'package:flutter_application_1/ui/friends/friends_page/friends_page_viewmodel.dart';
@@ -14,7 +16,7 @@ class FriendPageScreen extends StatefulWidget {
   FirstScreenState createState() => FirstScreenState();
 }
 
-class FirstScreenState extends State<FriendPageScreen>{
+class FirstScreenState extends State<FriendPageScreen> {
   Timer? timer;
 
   @override
@@ -30,111 +32,120 @@ class FirstScreenState extends State<FriendPageScreen>{
     timer?.cancel();
     super.dispose();
   }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar( 
-          centerTitle: true,
-          backgroundColor: const Color.fromRGBO(210, 102, 102, 1),
-          foregroundColor: Colors.black,
-          toolbarHeight: 115,
-          title: Text(
-            "Vänner",
-            textAlign: TextAlign.center,
-            style: GoogleFonts.itim(
-              textStyle: TextStyle(color: Colors.white, letterSpacing: 2),
-              fontSize: 70,
-            ),
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(210, 102, 102, 1),
+        foregroundColor: Colors.black,
+        toolbarHeight: 115,
+        title: Text(
+          "Vänner",
+          textAlign: TextAlign.center,
+          style: GoogleFonts.itim(
+            textStyle: TextStyle(color: Colors.white, letterSpacing: 2),
+            fontSize: 70,
           ),
         ),
-        body: Consumer<FriendsPageViewmodel>(
-          builder: (context, viewModel, _){
-            if(viewModel.users == null){
-              viewModel.refresh();
-              return const Center(child: CircularProgressIndicator());
-            }
-            if(viewModel.users!.isEmpty && viewModel.pendingRequests.isEmpty){
-              return Center(child: Text('No friends'));
-            }
-            final pendingRequests = viewModel.pendingRequests;
-            List<User> users = viewModel.users!;
-            return RefreshIndicator(
-              onRefresh: (){
-                return Future.delayed(Duration(milliseconds: 500),(){
-                  viewModel.refresh();
-                  setState((){});
-                });
-              },
-              child: ListView.builder(
-                itemCount: (pendingRequests.isNotEmpty)? users.length + pendingRequests.length + 2: users.length + 1,
-                itemBuilder: (context, index) {
-                  if(pendingRequests.isNotEmpty && index < pendingRequests.length+1){
-                    if(index == 0){
-                      return Text(
-                        " Vänförfrågningar",
-                        style: GoogleFonts.itim(
-                          textStyle: TextStyle(color: Colors.white, letterSpacing: 2),
-                          fontSize: 25,
+      ),
+      body: Consumer<FriendsPageViewmodel>(
+        builder: (context, viewModel, _) {
+          if (viewModel.users == null) {
+            viewModel.refresh();
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (viewModel.users!.isEmpty && viewModel.pendingRequests.isEmpty) {
+            return Center(child: Text('No friends'));
+          }
+          final pendingRequests = viewModel.pendingRequests;
+          List<User> users = viewModel.users!;
+          return RefreshIndicator(
+            onRefresh: () {
+              return Future.delayed(Duration(milliseconds: 500), () {
+                viewModel.refresh();
+                setState(() {});
+              });
+            },
+            child: ListView.builder(
+              itemCount:
+                  (pendingRequests.isNotEmpty)
+                      ? users.length + pendingRequests.length + 2
+                      : users.length + 1,
+              itemBuilder: (context, index) {
+                if (pendingRequests.isNotEmpty &&
+                    index < pendingRequests.length + 1) {
+                  if (index == 0) {
+                    return Text(
+                      " Vänförfrågningar",
+                      style: GoogleFonts.itim(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 2,
                         ),
-                      );
-                    }
-                    index-=1;
-                    return Center(
-                      child: UserCard(
-                        user: pendingRequests[index],
+                        fontSize: 25,
                       ),
                     );
-                    
                   }
-                  int value = (pendingRequests.isNotEmpty)? pendingRequests.length+1 : 0;
-                  index-= value;
-                  if(index == 0){
-                      return Text(
-                        " Mina vänner",
-                        style: GoogleFonts.itim(
-                          textStyle: TextStyle(color: Colors.white, letterSpacing: 2),
-                          fontSize: 25,
-                        ),
-                      );
-                  }
-                  index-=1;
-                  return Center(
-                    child: UserCard(
-                      user: users[index],
-                      toggleFavoriteFunction: (){
-                        viewModel.favourite(users[index].id);
-                      },
-                      removeFriendFunction: (){
-                        
-                        viewModel.removeFriend(users[index].id);
-                      },
+                  index -= 1;
+                  return Center(child: UserCard(user: pendingRequests[index]));
+                }
+                int value =
+                    (pendingRequests.isNotEmpty)
+                        ? pendingRequests.length + 1
+                        : 0;
+                index -= value;
+                if (index == 0) {
+                  return Text(
+                    " Mina vänner",
+                    style: GoogleFonts.itim(
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 2,
+                      ),
+                      fontSize: 25,
                     ),
                   );
-                } 
-              )
-            );
-          }
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: Color(0xFFFBDCDC),
-          shape: RoundedRectangleBorder(
-            side: BorderSide(width: 2, color: Color(0xFF371515), strokeAlign: 1),
-            borderRadius: BorderRadius.circular(90),
-          ),
-          onPressed: () {
-            context.push('/friends/search-users');
-          },
-          label: Text(
-            "     Lägg till ny vän    ",
-            style: GoogleFonts.itim(
-              textStyle: TextStyle(color: Colors.black),
-              fontSize: 32,
+                }
+                index -= 1;
+                return Center(
+                  child: UserCard(
+                    user: users[index],
+                    toggleFavoriteFunction: () {
+                      viewModel.favourite(users[index].id);
+                    },
+                    removeFriendFunction: () {
+                      viewModel.removeFriend(users[index].id);
+                    },
+                  ),
+                );
+              },
             ),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Color(0xFFFBDCDC),
+        shape: RoundedRectangleBorder(
+          side: BorderSide(width: 2, color: Color(0xFF371515), strokeAlign: 1),
+          borderRadius: BorderRadius.circular(90),
+        ),
+        onPressed: () {
+          context.push('/friends/search-users');
+        },
+        label: Text(
+          "     Lägg till ny vän    ",
+          style: GoogleFonts.itim(
+            textStyle: TextStyle(color: Colors.black),
+            fontSize: 32,
           ),
         ),
-        
-        backgroundColor: Color.fromRGBO(153, 88, 88, 1)
+      ),
+
+      backgroundColor: theme.Color(0xfffff8f7),
+      //Color.fromRGBO(153, 88, 88, 1),
     );
   }
 }
